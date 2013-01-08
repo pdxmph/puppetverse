533f499af603f7f361759a6ce130bf16cc9de983
Michael Hall <michael.hall@puppetlabs.com>
2013-01-07 16:17:07 -0800

    ntp stuff

--- files/site.pp
+++ files/site.pp
@@ -6,17 +6,10 @@ node default {
     content => "Welcome to ${::hostname}. This is a Puppet managed node in a Vagrant cluster.\n",
   }
 
-  #######################################################
-  # Any packages that should be applied to all nodes
-  # should be added to this array. If you want them
-  # to track the latest version, change the resource
-  # attribute to ensure => latest.
-  #######################################################
- # $pkgs = ["emacs23-nox"]
-
-#  package { $pkgs:
-#    ensure => present,
-#  }
+class { ntp:
+  ensure     => running,
+  autoupdate => true,
+}
 
 }
 
