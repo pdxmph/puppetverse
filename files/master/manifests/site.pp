eeb5e57d4901fa1fd794df85b8e70e4d0031e0a7
Michael Hall <michael.hall@puppetlabs.com>
2013-01-31 15:51:28 -0800

    WIP

--- files/master/manifests/site.pp
+++ files/master/manifests/site.pp
@@ -5,9 +5,6 @@ node default {
     ensure  => file,
     content => "Welcome to ${::hostname}. This is a Puppet-managed node in a Vagrant cluster.\n",
   }
-
   include hiera_sample
-
-  #include ntp
 }
 
