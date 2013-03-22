18f6674996825f8c33e4d5ba105b251a9c5a353b
Michael Hall <michael.hall@puppetlabs.com>
2013-03-21 17:24:01 -0700

    WIP: data sources for hierafied ntp

--- files/master/modules/hiera_sample/manifests/init.pp
+++ files/master/modules/hiera_sample/manifests/init.pp
@@ -1,18 +1,7 @@
-class hiera_sample($special_episode,$message){
-    # notify { $hiera_sample::message:}
-    # notify { $hiera_sample::domain: }
+class hiera_sample($special_episode,$message, $domain, $servers){
+     notify { $hiera_sample::message:}
+     notify { $hiera_sample::domain: }
+     notify { $hiera_sample::special_episode: }
+     notify { $servers: }
+}
 
-    $values = hiera_hash(hiera_sample::hashvalues, {"val1" => "somevalue1","val2" => "somevalue2","val3" => "somevalue3"}, "dceast.acme.com")
-    $ntp_servers = hiera_array(hiera_sample::servers, ["default1", "default2", "default3"], "dceast.acme.com")
-#    $values =
-#    $hash_vals = hiera_hash(hiera_sample::hashvalues)
-#     notify { $::hiera_sample::special_episode: }
-    notify { $ntp_servers: }
-#    notify { $::hiera_sample::hashvalues['val1']: }
-    # notify { a: message => $values['val1'] }
-    # notify { b: message => $values['val2'] }
-    # notify { c: message => $values['val3'] }
-    # notify { a: message => $::hiera_sample::hashvalues['val1'] }
- #    notify { b: message => $::hiera_sample::hashvalues['val2'] }
- #    notify { c: message => $::hiera_sample::hashvalues['val3'] }
-  }
