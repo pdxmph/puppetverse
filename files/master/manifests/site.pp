16238a29c0f0aaebb15af15038d4509bdcc04c5d
Michael Hall <michael.hall@puppetlabs.com>
2013-03-26 13:48:02 -0700

    WIP: Debian data source complete

--- files/master/manifests/site.pp
+++ files/master/manifests/site.pp
@@ -1,12 +1,5 @@
 node default {
- # notify { "Hello ${::hostname}!": }
-
-# file { '/etc/motd':
-#     ensure  => file,
-#     content => "Welcome to ${::hostname}. This is a Puppet-managed node in a Vagrant cluster.\n",
-#   }
-
-  include hiera_sample
+  #include hiera_sample
   #include ntp
 }
-
+hiera_include('classes')
